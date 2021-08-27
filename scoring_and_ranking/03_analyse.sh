53666431a2a72e91f727cd31fc3a468c8732b690
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-08-27 16:39:34 +0100

    add ranking analysis

--- scoring_and_ranking/03_analyse.sh
+++ scoring_and_ranking/03_analyse.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 
-mkdir -p analysis
-
 scripts="../CASF-scripts"
 data="../CASF-2016/power_scoring/CoreSet.dat"
 
-for model in $(ls logs)
+for power in "scoring" "ranking"
 do
-    echo "${model} Affinity"
-    python ${scripts}/scoring_power.py \
-        -c ${data} \
-        -s results/${model}_Affinity.dat \
-        -p negative \
-        -o results/${model}_Affinity \
-        > analysis/${model}_Affinity.log
+    mkdir -p analysis/${power}
+    mkdir -p results/${power}
 
-    python ${scripts}/ranking_power.py \
-        -c ${data} \
-        -s results/${model}_Affinity.dat \
-        -p negative \
-        -o results/${model}_Affinity \
-        > analysis/${model}_Affinity.log
-
-    for score in "CNNscore" "CNNaffinity"
+    for model in $(ls logs)
     do
-        echo "${model} ${score}"
-        python ${scripts}/scoring_power.py \
+        echo "${model} Affinity"
+        python ${scripts}/${power}_power.py \
             -c ${data} \
-            -s results/${model}_${score}.dat \
-            -p positive \
-            -o results/${model}_${score} \
-            > analysis/${model}_${score}.log
+            -s results/${model}_Affinity.dat \
+            -p negative \
+            -o results/${power}/${model}_Affinity \
+            > analysis/${power}/${model}_Affinity.log
+
+        for score in "CNNscore" "CNNaffinity"
+        do
+            echo "${model} ${score}"
+            python ${scripts}//${power}_power.py \
+                -c ${data} \
+                -s results/${model}_${score}.dat \
+                -p positive \
+                -o results/${power}/${model}_${score} \
+                > analysis/${power}/${model}_${score}.log
+        done
     done
 done
