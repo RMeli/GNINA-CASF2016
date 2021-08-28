f2cdcb855396c4695e7189d2bef7a120cd6b72ae
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-08-28 23:21:05 +0100

    add runs on optimised poses (vina and amber)

--- scoring_and_ranking/01_evaluate.sh
+++ scoring_and_ranking/01_evaluate.sh
@@ -9,9 +9,12 @@ mkdir -p logs
 
 models="default redock_default2018 general_default2018 crossdock_default2018 dense"
 
+# No optimisation
+optname="noopt"
+mkdir -p logs/${optname}
 for model in ${models}
 do
-    mkdir -p logs/${model}
+    mkdir -p logs/${optname}/${model}
 
     for system in $(tail -n +2 ${data} | awk '{print $1}')
     do
@@ -24,6 +27,53 @@ do
             --autobox_ligand ${ligand} \
             --score_only \
             --cnn "${model}_ensemble" \
-            > logs/${model}/${system}.log
+            --seed 42 \
+            > logs/${optname}/${model}/${system}.log
+    done
+done
+
+# Amber (CASF2016) optimisation
+optname="amberopt"
+mkdir -p logs/${optname}
+for model in ${models}
+do
+    mkdir -p logs/${optname}/${model}
+
+    for system in $(tail -n +2 ${data} | awk '{print $1}')
+    do
+        ligand="${coreset}/${system}/${system}_ligand_opt.mol2"
+        receptor="${coreset}/${system}/${system}_protein.pdb"
+
+        singularity run --nv --app gnina ${container} \
+            -l ${ligand} \
+            -r ${receptor} \
+            --autobox_ligand ${ligand} \
+            --score_only \
+            --cnn "${model}_ensemble" \
+            --seed 42 \
+            > logs/${optname}/${model}/${system}.log
+    done
+done
+
+# Vina optimisation
+optname="vinaopt"
+mkdir -p logs/${optname}
+for model in ${models}
+do
+    mkdir -p logs/${optname}/${model}
+
+    for system in $(tail -n +2 ${data} | awk '{print $1}')
+    do
+        ligand="${coreset}/${system}/${system}_ligand.mol2"
+        receptor="${coreset}/${system}/${system}_protein.pdb"
+
+        singularity run --nv --app gnina ${container} \
+            -l ${ligand} \
+            -r ${receptor} \
+            --autobox_ligand ${ligand} \
+            --minimize \
+            --cnn "${model}_ensemble" \
+            --seed 42 \
+            > logs/${optname}/${model}/${system}.log
     done
 done
