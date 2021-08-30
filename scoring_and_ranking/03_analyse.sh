58991ba766b64f9324f7803a6321eb37cbc70b66
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-08-30 11:06:34 +0100

    add analysis for everything

--- scoring_and_ranking/03_analyse.sh
+++ scoring_and_ranking/03_analyse.sh
@@ -5,28 +5,31 @@ data="../CASF-2016/power_scoring/CoreSet.dat"
 
 for power in "scoring" "ranking"
 do
-    mkdir -p analysis/logs/${power}
-    mkdir -p analysis/outputs/${power}
-
-    for model in $(ls logs)
+    for optname in "noopt" "amberopt" "vinaopt"
     do
-        echo "${model} Affinity"
-        python ${scripts}/${power}_power.py \
-            -c ${data} \
-            -s results/${model}_Affinity.dat \
-            -p negative \
-            -o results/${power}/${model}_Affinity \
-            > analysis/${power}/${model}_Affinity.log
-
-        for score in "CNNscore" "CNNaffinity"
+        mkdir -p analysis/logs/${power}/${optname}
+        mkdir -p analysis/outputs/${power}/${optname}
+        
+        for model in $(ls logs/${optname})
         do
-            echo "${model} ${score}"
-            python ${scripts}//${power}_power.py \
+            echo "${optname} ${model} Affinity"
+            python ${scripts}/${power}_power.py \
                 -c ${data} \
-                -s results/${model}_${score}.dat \
-                -p positive \
-                -o results/${power}/${model}_${score} \
-                > analysis/${power}/${model}_${score}.log
+                -s results/${optname}/${model}_Affinity.dat \
+                -p negative \
+                -o analysis/outputs/${power}/${optname}/${model}_Affinity \
+                > analysis/logs/${power}/${optname}/${model}_Affinity.log
+
+            for score in "CNNscore" "CNNaffinity"
+            do
+                echo "${optname} ${model} ${score}"
+                python ${scripts}/${power}_power.py \
+                    -c ${data} \
+                    -s results/${optname}/${model}_${score}.dat \
+                    -p positive \
+                    -o analysis/outputs/${power}/${optname}/${model}_${score} \
+                    > analysis/logs/${power}/${optname}/${model}_${score}.log
+            done
         done
     done
 done
